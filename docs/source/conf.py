84ef3d6036f94342a5436f3af522d7e7a7aa9cda
David Zwicker <david.zwicker@ds.mpg.de>
2020-03-18 15:50:29 +0100

    Remove movie example from gallery

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -253,6 +253,7 @@ sphinx_gallery_conf = {
      'examples_dirs': '../../examples',
      'gallery_dirs': 'examples_gallery',
      'filename_pattern': '/',
+     'ignore_pattern': r'.*movie.*',
      'capture_repr': (),
 }
 
