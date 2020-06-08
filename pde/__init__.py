da4fb0b83b86d569a284191d34f70b8038855f66
David Zwicker <david.zwicker@ds.mpg.de>
2020-06-08 17:53:11 +0200

    Bumped version to 0.8.2

--- pde/__init__.py
+++ pde/__init__.py
@@ -3,7 +3,7 @@ The py-pde package provides classes and methods for solving partial differential
 equations.
 """
 
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 
 from .fields import *  # @UnusedWildImport
 from .grids import *  # @UnusedWildImport
