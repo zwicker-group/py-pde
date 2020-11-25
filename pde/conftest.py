50057434e3c52f0fb544da968d7fcf8131559c97
David Zwicker <david.zwicker@ds.mpg.de>
2020-11-25 13:10:47 +0100

    Improved testing

--- pde/conftest.py
+++ pde/conftest.py
@@ -9,13 +9,6 @@ import numpy as np
 import pytest
 
 
-@pytest.fixture(scope="session", autouse=True)
-def enable_raise_errors():
-    """ helper function enabling errors for all tests """
-    # prepare something ahead of all tests
-    np.seterr(all="raise", under="ignore")
-
-
 @pytest.fixture(scope="function", autouse=True)
 def suppressing():
     """ helper function adjusting message reporting for all tests """
