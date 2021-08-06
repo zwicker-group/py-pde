Fixed a bug in the cylindrical implementation of the squared gradient operator

pde/grids/operators/cartesian.py
@@ -626,7 +626,7 @@ def _make_gradient_squared_numba_2d(
 
     if central:
         # use central differences
-        scale_x, scale_y = 1 / (2 * grid.discretization) ** 2
+        scale_x, scale_y = 0.25 / grid.discretization ** 2
 
         @jit(parallel=parallel)
         def gradient_squared(arr: np.ndarray, out: np.ndarray) -> None:
@@ -639,7 +639,7 @@ def _make_gradient_squared_numba_2d(
 
     else:
         # use forward and backward differences
-        scale_x, scale_y = 1 / (2 * grid.discretization ** 2)
+        scale_x, scale_y = 0.5 / grid.discretization ** 2
 
         @jit(parallel=parallel)
         def gradient_squared(arr: np.ndarray, out: np.ndarray) -> None:

pde/grids/operators/cylindrical_sym.py
@@ -118,24 +118,24 @@ def make_gradient_squared(
     """
     # use processing for large enough arrays
     dim_r, dim_z = grid.shape
-    scale_r, scale_z = 1 / (2 * grid.discretization) ** 2
     parallel = dim_r * dim_z >= config["numba.parallel_threshold"]
 
     if central:
         # use central differences
+        scale_r, scale_z = 0.25 / grid.discretization ** 2
 
         @jit(parallel=parallel)
         def gradient_squared(arr: np.ndarray, out: np.ndarray) -> None:
             """apply gradient operator to array `arr`"""
             for i in nb.prange(1, dim_r + 1):  # iterate radial points
                 for j in range(1, dim_z + 1):  # iterate axial points
-                    arr_z_l, arr_z_h = arr[i, j - 1], arr[i, j + 1]
                     term_r = (arr[i + 1, j] - arr[i - 1, j]) ** 2
-                    term_z = (arr_z_h - arr_z_l) ** 2
+                    term_z = (arr[i, j + 1] - arr[i, j - 1]) ** 2
                     out[i, j] = term_r * scale_r + term_z * scale_z
 
     else:
         # use forward and backward differences
+        scale_r, scale_z = 0.5 / grid.discretization ** 2
 
         @jit(parallel=parallel)
         def gradient_squared(arr: np.ndarray, out: np.ndarray) -> None:

pde/grids/operators/polar_sym.py
@@ -116,7 +116,7 @@ def make_gradient_squared(grid: PolarSymGrid, central: bool = True) -> OperatorT
 
     if central:
         # use central differences
-        scale = 1 / (2 * dr) ** 2
+        scale = 0.25 / dr
 
         @jit
         def gradient_squared(arr: np.ndarray, out: np.ndarray) -> None:
@@ -126,7 +126,7 @@ def make_gradient_squared(grid: PolarSymGrid, central: bool = True) -> OperatorT
 
     else:
         # use forward and backward differences
-        scale = 1 / (2 * dr ** 2)
+        scale = 0.5 / dr ** 2
 
         @jit
         def gradient_squared(arr: np.ndarray, out: np.ndarray) -> None:

pde/grids/operators/spherical_sym.py
@@ -139,7 +139,7 @@ def make_gradient_squared(grid: SphericalSymGrid, central: bool = True) -> Opera
 
     if central:
         # use central differences
-        scale = 1 / (2 * dr) ** 2
+        scale = 0.25 / dr ** 2
 
         @jit
         def gradient_squared(arr: np.ndarray, out: np.ndarray) -> None:
@@ -149,7 +149,7 @@ def make_gradient_squared(grid: SphericalSymGrid, central: bool = True) -> Opera
 
     else:
         # use forward and backward differences
-        scale = 1 / (2 * dr ** 2)
+        scale = 0.5 / dr ** 2
 
         @jit
         def gradient_squared(arr: np.ndarray, out: np.ndarray) -> None: