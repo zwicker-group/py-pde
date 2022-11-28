01799a5f692651c77f68d61dcb9e1ea0b3f16741
David Zwicker <david.zwicker@ds.mpg.de>
2022-11-28 17:59:31 +0100

    Added versioneer to manage the versions

--- pde/version.py
+++
@@ -1,7 +0,0 @@
-"""
-Contains the version information of the package
-
-.. codeauthor:: David Zwicker <david.zwicker@ds.mpg.de>
-"""
-
-__version__ = "0.23.0"
--- scripts/templates/setup.py
+++ scripts/templates/setup.py
@@ -1,51 +1,23 @@
 from pathlib import Path
 
+import versioneer
 from setuptools import find_packages, setup
 
-BASE_PATH = Path(__file__).resolve().parent
-
-# read the version from the particular file
-with open(BASE_PATH / "pde" / "version.py", "r") as f:
-    exec(f.read())
-
-DOWNLOAD_URL = f"https://github.com/zwicker-group/py-pde/archive/v{__version__}.tar.gz"
+# determine the version of the package
+version = versioneer.get_version()
 
 # read the description from the README file
+BASE_PATH = Path(__file__).resolve().parent
 with open(BASE_PATH / "README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="py-pde",
     packages=find_packages(),
     include_package_data=True,  # include template files and the like
     zip_safe=False,  # this is required for mypy to find the py.typed file
-    version=__version__,
-    license="MIT",
-    description="Python package for solving partial differential equations",
+    version=version,
+    cmdclass=versioneer.get_cmdclass(),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="David Zwicker",
-    author_email="david.zwicker@ds.mpg.de",
-    url="https://github.com/zwicker-group/py-pde",
-    download_url=DOWNLOAD_URL,
-    keywords=["pdes", "partial-differential-equations", "dynamical-systems"],
-    python_requires=">=3.8",
-    install_requires=$INSTALL_REQUIRES,
-    extras_require={
-        "hdf": ["$h5py"],
-        "io": ["$h5py", "$pandas"],
-        "interactive": ["$napari", "$ipywidgets"],
-        "mpi": ["$numba_mpi", "$mpi4py"],
-    },
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
+    download_url=f"https://github.com/zwicker-group/py-pde/archive/{version}.tar.gz",
 )
