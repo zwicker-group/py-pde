a5140976eb4bb84d567d0780b1fe2774aecbd76e
David Zwicker <david.zwicker@ds.mpg.de>
2022-11-28 18:03:16 +0100

    Fixed circular import

--- pde/solvers/controller.py
+++ pde/solvers/controller.py
@@ -16,7 +16,6 @@ from ..trackers.base import (
     TrackerCollection,
     TrackerCollectionDataType,
 )
-from ..version import __version__
 from .base import SolverBase
 
 if TYPE_CHECKING:
@@ -65,6 +64,8 @@ class Controller:
                 detail. In particular, the interval at which the tracker is evaluated
                 can be chosen when creating a tracker object explicitly.
         """
+        from .. import __version__  # late import to prevent circular import
+
         self.solver = solver
         self.t_range = t_range  # type: ignore
         self.trackers = TrackerCollection.from_data(tracker)
