d8925b27ec5014fd3a6c22148b595e061dc5cf4f
David Zwicker <david.zwicker@ds.mpg.de>
2024-01-02 16:12:40 +0100

    Improved documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -110,6 +110,7 @@ html_theme_options = {
     #     'includehidden': True,
     #     'titles_only': False
 }
+html_secnumber_suffix = " "
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
