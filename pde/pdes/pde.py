21a1f07887775b285c878eb013fbfd6c72203b9f
David Zwicker <david.zwicker@ds.mpg.de>
2024-03-02 08:18:05 +0100

    Improved error message for `bc_ops` argument

--- pde/pdes/pde.py
+++ pde/pdes/pde.py
@@ -185,11 +185,13 @@ class PDE(PDEBase):
         # setup boundary conditions
         if bc_ops is None:
             bcs = {"*:*": bc}
-        else:
+        elif isinstance(bcs, dict):
             bcs = dict(bc_ops)
             if "*:*" in bcs and bc != "auto_periodic_neumann":
                 self._logger.warning("Found default BCs in `bcs` and `bc_ops`")
             bcs["*:*"] = bc  # append default boundary conditions
+        else:
+            raise TypeError('`bc_ops` must be a dictionary')
 
         self.bcs: dict[str, Any] = {}
         for key_str, value in bcs.items():
