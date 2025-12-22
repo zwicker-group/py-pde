929094aae8c3cf0226a89d6b11191f67dfbe80a6
David Zwicker <david.zwicker@ds.mpg.de>
2025-12-22 16:56:06 +0100

    Refactor solver references from ExplicitSolver to EulerSolver in documentation, examples, and tests

--- tests/solvers/test_explicit_solvers.py
+++ tests/solvers/test_explicit_solvers.py
@@ -7,26 +7,25 @@ import pytest
 
 from pde import PDE, DiffusionPDE, MemoryStorage, ScalarField, UnitGrid
 from pde.pdes import PDEBase
-from pde.solvers import Controller, ExplicitMPISolver, ExplicitSolver
+from pde.solvers import Controller, EulerSolver, ExplicitMPISolver, RungeKuttaSolver
 from pde.tools import mpi
 
 
 @pytest.mark.multiprocessing
-@pytest.mark.parametrize("scheme", ["euler", "runge-kutta"])
 @pytest.mark.parametrize("backend", ["numpy", "numba"])
-def test_solvers_simple_fixed(scheme, backend):
+def test_solvers_simple_fixed(backend):
     """Test explicit solvers."""
     grid = UnitGrid([4])
     xs = grid.axes_coords[0]
     field = ScalarField.from_expression(grid, "x")
     eq = PDE({"c": "c"})
 
-    dt = 0.001 if scheme == "euler" else 0.01
+    dt = 0.001
 
     if mpi.parallel_run:
-        solver = ExplicitMPISolver(eq, scheme=scheme, backend=backend, adaptive=False)
+        solver = ExplicitMPISolver(eq, backend=backend, adaptive=False)
     else:
-        solver = ExplicitSolver(eq, scheme=scheme, backend=backend, adaptive=False)
+        solver = EulerSolver(eq, backend=backend, adaptive=False)
     controller = Controller(solver, t_range=10.0, tracker=None)
     res = controller.run(field, dt=dt)
 
@@ -37,22 +36,20 @@ def test_solvers_simple_fixed(scheme, backend):
 
 
 @pytest.mark.multiprocessing
-@pytest.mark.parametrize("scheme", ["euler", "runge-kutta"])
 @pytest.mark.parametrize("backend", ["numpy", "numba"])
-def test_solvers_simple_adaptive(scheme, backend):
+def test_solvers_simple_adaptive(backend):
     """Test explicit solvers."""
     grid = UnitGrid([4])
     y0 = np.array([1e-3, 1e-3, 1e3, 1e3])
     field = ScalarField(grid, y0)
     eq = PDE({"c": "c"})
+    dt = 0.1
 
-    dt = 0.1 if scheme == "euler" else 1
-
-    args = {"scheme": scheme, "backend": backend, "adaptive": True, "tolerance": 1e-1}
+    args = {"backend": backend, "adaptive": True, "tolerance": 1e-1}
     if mpi.parallel_run:
         solver = ExplicitMPISolver(eq, **args)
     else:
-        solver = ExplicitSolver(eq, **args)
+        solver = EulerSolver(eq, **args)
     storage = MemoryStorage()
     controller = Controller(solver, t_range=10.1, tracker=storage.tracker(1.0))
     res = controller.run(field, dt=dt)
@@ -61,25 +58,22 @@ def test_solvers_simple_adaptive(scheme, backend):
         np.testing.assert_allclose(res.data, y0 * np.exp(10.1), rtol=0.02)
         assert solver.info["steps"] != pytest.approx(10.1 / dt, abs=1)
         assert solver.info["dt_adaptive"]
-        if scheme == "euler":
-            assert solver.info["dt_statistics"]["min"] < 0.0005
-        else:
-            assert solver.info["dt_statistics"]["min"] < 0.03
+        assert solver.info["dt_statistics"]["min"] < 0.0005
         assert np.allclose(storage.times, np.arange(11))
 
 
-@pytest.mark.parametrize("scheme", ["euler", "runge-kutta"])
+@pytest.mark.parametrize("solver", [EulerSolver, RungeKuttaSolver])
 @pytest.mark.parametrize("adaptive", [True, False])
-def test_solvers_time_dependent(scheme, adaptive):
+def test_solvers_time_dependent(solver, adaptive):
     """Test explicit solvers with a simple ODE."""
     grid = UnitGrid([1])
     field = ScalarField(grid, 1)
     eq = PDE({"y": "2*sin(t) - y"})
 
-    dt = 1e-3 if scheme == "euler" else 1e-2
+    dt = 1e-3 if solver is EulerSolver else 1e-2
 
     storage = MemoryStorage()
-    solver = ExplicitSolver(eq, scheme=scheme, adaptive=adaptive)
+    solver = solver(eq, adaptive=adaptive)
     controller = Controller(solver, t_range=20.0, tracker=storage.tracker(1))
     controller.run(field, dt=dt)
 
@@ -101,11 +95,11 @@ def test_stochastic_solvers(backend, rng):
     eq = DiffusionPDE()
     seq = DiffusionPDE(noise=1e-10)
 
-    solver1 = ExplicitSolver(eq, backend=backend)
+    solver1 = EulerSolver(eq, backend=backend)
     c1 = Controller(solver1, t_range=1, tracker=None)
     s1 = c1.run(field, dt=1e-3)
 
-    solver2 = ExplicitSolver(seq, backend=backend)
+    solver2 = EulerSolver(seq, backend=backend)
     c2 = Controller(solver2, t_range=1, tracker=None)
     s2 = c2.run(field, dt=1e-3)
 
@@ -122,7 +116,7 @@ def test_stochastic_adaptive_solver(caplog, rng):
     field = ScalarField.random_uniform(UnitGrid([16]), -1, 1, rng=rng)
     eq = DiffusionPDE(noise=1e-6)
 
-    solver = ExplicitSolver(eq, backend="numpy", adaptive=True)
+    solver = EulerSolver(eq, backend="numpy", adaptive=True)
     c = Controller(solver, t_range=1, tracker=None)
     with pytest.raises(RuntimeError):
         c.run(field, dt=1e-2)
@@ -134,13 +128,13 @@ def test_unsupported_stochastic_solvers(rng):
     eq = DiffusionPDE(noise=1)
 
     with pytest.raises(RuntimeError):
-        eq.solve(field, 1, solver="explicit", scheme="runge-kutta", tracker=None)
+        eq.solve(field, 1, solver="runge-kutta", tracker=None)
     with pytest.raises(RuntimeError):
         eq.solve(field, 1, solver="scipy", scheme="runge-kutta", tracker=None)
 
 
-@pytest.mark.parametrize("scheme", ["euler", "runge-kutta"])
-def test_adaptive_solver_nan(scheme):
+@pytest.mark.parametrize("solver", ["euler", "runge-kutta"])
+def test_adaptive_solver_nan(solver):
     """Test whether the adaptive solver can treat nans."""
     # Note for programmer: A similar test for the `numba` backend is difficult to
     # implement, since we only want to fail very rarely. We tried doing it with random
@@ -163,9 +157,8 @@ def test_adaptive_solver_nan(scheme):
         field,
         1,
         dt=0.1,
-        solver="explicit",
+        solver=solver,
         backend="numpy",
-        scheme=scheme,
         tracker=None,
         adaptive=True,
         ret_info=True,
