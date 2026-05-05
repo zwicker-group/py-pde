Refactor Gaussian noise generation across backends to fix scaling by cell volumes (#839)

pde/backends/jax/backend.py
@@ -753,8 +753,6 @@ class JaxBackend(BackendBase[jax.Array]):
     ) -> Callable[[], jax.Array]:
         """Create a function generating Gaussian white noise.
 
-        This noise is already scaled to respect different cell volumes of the grid.
-
         Args:
             field (:class:`~pde.fields.base.FieldBase`):
                 An example for the state from which the grid and other information can
@@ -767,7 +765,6 @@ class JaxBackend(BackendBase[jax.Array]):
         from jax.experimental.random import stateful_rng
 
         data_shape: tuple[int, ...] = field.data.shape
-        scale = np.sqrt(1 / field.grid.cell_volumes)
 
         # initialize jax random number generator with numpy random number
         seed = rng.integers(0, np.iinfo(np.uint32).max + 1)
@@ -777,7 +774,7 @@ class JaxBackend(BackendBase[jax.Array]):
         def gaussian_noise() -> jax.Array:
             """Helper function returning a noise realization."""
             key = jax_rng.key()
-            return scale * jrandom.normal(key, shape=data_shape)  # type: ignore
+            return jrandom.normal(key, shape=data_shape)
 
         return gaussian_noise
 

pde/backends/numba/backend.py
@@ -1455,8 +1455,6 @@ class NumbaBackend(NumpyBackend):
     ) -> Callable[[], NumericArray]:
         """Create a function generating Gaussian white noise.
 
-        This noise is already scaled to respect different cell volumes of the grid.
-
         Args:
             field (:class:`~pde.fields.base.FieldBase`):
                 An example for the state from which the grid and other information can
@@ -1466,12 +1464,11 @@ class NumbaBackend(NumpyBackend):
                 Not used in this numba backend.
         """
         data_shape: tuple[int, ...] = field.data.shape
-        scale = np.sqrt(1 / field.grid.cell_volumes)
 
         @self.compile_function
         def gaussian_noise() -> NumericArray:
             """Generate Gaussian white noise."""
-            return scale * np.random.randn(*data_shape)  # type: ignore
+            return np.random.randn(*data_shape)
 
         return gaussian_noise
 

pde/backends/torch/backend.py
@@ -589,8 +589,6 @@ class TorchBackend(BackendBase[torch.Tensor]):
     ) -> Callable[[], torch.Tensor]:
         """Create a function generating Gaussian white noise.
 
-        This noise is already scaled to respect different cell volumes of the grid.
-
         Args:
             field (:class:`~pde.fields.base.FieldBase`):
                 An example for the state from which the grid and other information can
@@ -602,15 +600,11 @@ class TorchBackend(BackendBase[torch.Tensor]):
         from .utils import TorchGaussianNoise
 
         data_shape: tuple[int, ...] = field.data.shape
-        scale = np.sqrt(1 / field.grid.cell_volumes)
         generator = torch.Generator(device=self.device)
         generator.manual_seed(int(rng.integers(0, 2**32)))
 
         return TorchGaussianNoise(
-            data_shape,
-            dtype=self.get_numpy_dtype(field.dtype),
-            scale=scale,
-            generator=generator,
+            data_shape, dtype=self.get_numpy_dtype(field.dtype), generator=generator
         ).to(self.device)
 
     def make_stepper(self, solver: SolverBase, state: TField) -> StepperType:

pde/solvers/euler.py
@@ -104,6 +104,8 @@ class EulerSolver(AdaptiveSolverBase):
 
         # noise increment scales with square root of time step
         dt_sqrt = np.sqrt(dt)
+        # noise variance scales with inverse cell volumes
+        inv_cell = 1 / state.grid.cell_volumes
 
         def single_step(state_data: NumericArray, t: float) -> NumericArray:
             """Perform a single Euler-Maruyama step."""
@@ -129,11 +131,13 @@ class EulerSolver(AdaptiveSolverBase):
 
             if use_noise_variance:
                 dW = gaussian_noise()
-                state_data += dt_sqrt * nx.sqrt(noise_var_field) * dW
+                state_data += dt_sqrt * nx.sqrt(noise_var_field * inv_cell) * dW
 
                 # add a drift term if the interpretation is not Itô
                 if has_noise_drift_term:
-                    state_data += 0.5 * dt * noise_drift_factor * noise_var_diff_field
+                    state_data += (
+                        0.5 * dt * noise_drift_factor * noise_var_diff_field * inv_cell
+                    )
 
             return state_data
 

tests/solvers/test_explicit_solvers.py
@@ -6,7 +6,7 @@ import numpy as np
 import pytest
 from scipy import stats
 
-from pde import PDE, DiffusionPDE, MemoryStorage, ScalarField, UnitGrid
+from pde import PDE, CartesianGrid, DiffusionPDE, MemoryStorage, ScalarField, UnitGrid
 from pde.pdes import PDEBase
 from pde.solvers import (
     Controller,
@@ -188,6 +188,7 @@ def test_stochastic_solver_equilibrium(
     backend, solver_cls, mobility_factor, interpretation, rng
 ):
     """Test wether conversion to Itô representation is correct."""
+    size_scale = 2.3  # cell volume
     k_val = 0.5  # stiffness of the potential
 
     class MultiplicativeNoise(PDE):
@@ -222,18 +223,18 @@ def test_stochastic_solver_equilibrium(
     eq = MultiplicativeNoise(k=k_val, rng=rng, noise_interpretation=interpretation)
 
     # simulate an independent ensemble of points
-    field = ScalarField(UnitGrid([1024]))
+    field = ScalarField(CartesianGrid([[0, 2048 * size_scale]], 2048))
     solver = solver_cls(eq, backend=backend)
     controller = Controller(solver, t_range=5)
     result = controller.run(field, dt=1e-3)
 
     # check whether the points exhibit the expected distribution
-    sigma = 1 / np.sqrt(k_val)
+    sigma = 1 / np.sqrt(k_val * size_scale)
     test_res = stats.kstest(result.data, "norm", args=(0, sigma))
     if interpretation == "ito" and mobility_factor != 0:
-        assert test_res.pvalue < 0.05  # expect different distribution
+        assert test_res.pvalue < 0.04  # expect different distribution
     else:
-        assert test_res.pvalue > 0.05  # expect Gaussian distribution
+        assert test_res.pvalue > 0.04  # expect Gaussian distribution
 
 
 def test_stochastic_adaptive_solver(caplog, rng):