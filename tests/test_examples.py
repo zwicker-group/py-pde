dd312225c3073e0bb7b4bb5cd6cb90c304918ae8
David Zwicker <david.zwicker@ds.mpg.de>
2026-07-26 10:26:50 +0200

    Refactor parameterization in tests to use lists for improved compatibility and clarity

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -14,8 +14,8 @@ from pde.tools.misc import module_available
 from pde.visualization.movies import Movie
 
 PACKAGE_PATH = Path(__file__).resolve().parents[1]
-EXAMPLES = (PACKAGE_PATH / "examples").glob("*/*.py")
-NOTEBOOKS = (PACKAGE_PATH / "examples").glob("*/*.ipynb")
+EXAMPLES = list((PACKAGE_PATH / "examples").glob("*/*.py"))
+NOTEBOOKS = list((PACKAGE_PATH / "examples").glob("*/*.ipynb"))
 
 SKIP_EXAMPLES: list[str] = []
 if not Movie.is_available():
